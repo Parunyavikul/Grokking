b9a0e5a14dd5526b7834c6c3982a7068babc3ba7
Patty Arunyavikul <parunyavikul@gmail.com>
2021-06-06 22:54:31 -0400

    Completed Sliding Window 3

---
+++ SlidingWindow/LongestSubstringK.js
@@ -0,0 +1,13 @@
+/* eslint-disable no-unused-vars */
+/**
+ * Given a string, find the length of th elongest substring in it with no more than K distinct characters.
+ */
+
+function longest_substring_no_more_than_k_distinct_chars(str, k){
+  let maxLength =0,
+      start = 0,
+      charFreq = {};
+
+
+  return maxLength;
+}
